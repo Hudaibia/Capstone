43ea54aff6135454a8562e521992c88199a3588a
Hudaibia <86105967+Hudaibia@users.noreply.github.com>
2022-03-25 20:49:43 +0500

    Prototype
    A basic prototype

---
+++ Assets/Scripts/NewPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayer : PhysicsObject
+{
+
+
+    [SerializeField] private float maxSpeed = 1;
+    [SerializeField] private float jumpPower = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = new Vector2(Input.GetAxis("Horizontal")* maxSpeed,0);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpPower;
+        }
+        
+    }
+}
